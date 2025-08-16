1173ac47d02e041a24a77090fce25f2b5e4faafb
shazomii <seyeoyeniran2u@gmail.com>
2025-08-16 12:10:19 +0100

    Configure for npm publishing

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        library: 'image-carousel',
+        libraryTarget: 'umd',
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
         }),
     ],
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
     module: {
         rules: [
             {
